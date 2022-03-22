12fff5e2467e4c4a40c6e12b4a99f457c054394b
Halvor <stektpotet@gmail.com>
2022-03-22 21:53:24 +0100

    Add batch norm

--- assignment4/SSD/ssd/modeling/backbones/basic4a.py
+++ assignment4/SSD/ssd/modeling/backbones/basic4a.py
@@ -5,19 +5,15 @@ from typing import Tuple, List
 from torch import nn
 
 class BasicBlock(nn.Sequential):
-    def __init__(self, in_channels: int, mid_channels: int, out_channels: int, out_stride: int = 2, out_padding=1, use_bn=False):
-        layers = [
+    def __init__(self, in_channels: int, mid_channels: int, out_channels: int, out_stride: int = 2, out_padding=1):
+        super(BasicBlock, self).__init__(OrderedDict([
             ('relu0', nn.ReLU()),
-            ('conv1', nn.Conv2d(in_channels, mid_channels, kernel_size=3, stride=1, padding=1))
-        ]
-        if use_bn:
-            layers.append(('bn1', nn.BatchNorm2d(mid_channels)))
-        layers.append(('relu1', nn.ReLU()))
-        layers.append(('conv2', nn.Conv2d(mid_channels, out_channels, kernel_size=3, stride=out_stride, padding=out_padding)))
-        if use_bn:
-            layers.append(('bn2', nn.BatchNorm2d(out_channels)))
-        layers.append(('relu2', nn.ReLU()))
-        super(BasicBlock, self).__init__(OrderedDict(layers))
+            ('conv1', nn.Conv2d(in_channels, mid_channels, kernel_size=3, stride=1, padding=1)),
+            ('relu1', nn.ReLU()),
+            ('conv2', nn.Conv2d(mid_channels, out_channels, kernel_size=3, stride=out_stride, padding=out_padding)),
+            ('relu2', nn.ReLU())
+        ]))
+
 
 class BasicModel(nn.Module):
     """
@@ -33,32 +29,22 @@ class BasicModel(nn.Module):
     def __init__(self,
             output_channels: List[int],
             image_channels: int,
-            output_feature_sizes: List[Tuple[int]], use_bn=True):
+            output_feature_sizes: List[Tuple[int]]):
         super().__init__()
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
-
-        features1 = [
-            nn.Conv2d(image_channels, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(), nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(), nn.MaxPool2d(2, 2),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(),
-            nn.Conv2d(64, output_channels[0], kernel_size=3, stride=2, padding=1), nn.ReLU()
-        ]
-        if use_bn:
-            features1.insert(1, nn.BatchNorm2d(32))
-            features1.insert(5, nn.BatchNorm2d(64))
-            features1.insert(8, nn.BatchNorm2d(64))
-            features1.insert(11, nn.BatchNorm2d(output_channels[0]))
-
-
         self.features = nn.Sequential(OrderedDict([
-            ('features1', nn.Sequential(*features1)),
-            ('features2', BasicBlock(output_channels[0], 128, output_channels[1], use_bn=use_bn)),
-            ('features3', BasicBlock(output_channels[1], 256, output_channels[2], use_bn=use_bn)),
-            ('features4', BasicBlock(output_channels[2], 128, output_channels[3], use_bn=use_bn)),
-            ('features5', BasicBlock(output_channels[3], 128, output_channels[4], use_bn=use_bn)),
-            ('features6', BasicBlock(output_channels[4], 128, output_channels[5], 1, 0, use_bn=use_bn))
+            ('features1', nn.Sequential(
+                nn.Conv2d(image_channels, 32, kernel_size=3, stride=1, padding=1), nn.ReLU(), nn.MaxPool2d(2, 2),
+                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(), nn.MaxPool2d(2, 2),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1), nn.ReLU(),
+                nn.Conv2d(64, output_channels[0], kernel_size=3, stride=2, padding=1), nn.ReLU()
+            )),
+            ('features2', BasicBlock(output_channels[0], 128, output_channels[1])),
+            ('features3', BasicBlock(output_channels[1], 256, output_channels[2])),
+            ('features4', BasicBlock(output_channels[2], 128, output_channels[3])),
+            ('features5', BasicBlock(output_channels[3], 128, output_channels[4])),
+            ('features6', BasicBlock(output_channels[4], 128, output_channels[5], out_stride=1, out_padding=0)),
         ]))
 
     def forward(self, x) -> Tuple[torch.Tensor, ...]:
